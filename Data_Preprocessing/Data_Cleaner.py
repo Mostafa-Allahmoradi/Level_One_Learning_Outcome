a4f926f0f4f2a1f88111a52ed6010a5e2d0e2975
Mostafa-Allahmoradi <mostafa.allahmoradi2018@gmail.com>
2025-12-31 13:57:45 -0500

    Handled missing values and removed the highly correlated features. Also did some feature engineering.

--- Data_Preprocessing/Data_Cleaner.py
+++ Data_Preprocessing/Data_Cleaner.py
@@ -1,17 +1,29 @@
+from sklearn.impute import KNNImputer
 
 class DataCleaner:
     def __init__(self, df):
         self.df = df
+        
+    def get_features_with_missing_values(self) -> list:
+        """Identify features that contain at least one missing value."""
+        columns_with_missing_values = self.df.columns[self.df.isnull().any()]
+        return columns_with_missing_values.tolist()
 
-    def handle_missing_values(self, strategy='mean', column=None):
+    def handle_missing_values(self, strategy='mean'):
         if strategy == 'mean':
-            self.df.fillna(self.df[column].mean(), inplace=True)
+            self.df.fillna(self.df.mean(), inplace=True)
         elif strategy == 'median':
-            self.df.fillna(self.df[column].median(), inplace=True)
+            self.df.fillna(self.df.median(), inplace=True)
         elif strategy == 'mode':
-            self.df.fillna(self.df[column].mode().iloc[0], inplace=True)
+            self.df.fillna(self.df.mode().iloc[0], inplace=True)
+        elif strategy == 'drop_rows':
+            self.df.dropna(inplace=True)
+        elif strategy == 'knn': # Using KNN Imputer for one specific column
+            imputer = KNNImputer(n_neighbors=5) # Neighbors can be adjusted but should be an odd number
+            imputer.fit(self.df)
+            imputer.transform(self.df)         
         else:
-            raise ValueError("Strategy not recognized. Use 'mean', 'median', or 'mode'.")
+            raise ValueError("Strategy not recognized. Use 'mean', 'median', 'mode', 'drop_rows', or 'knn'.")
 
     def remove_duplicates(self):
         self.df.drop_duplicates(inplace=True)
